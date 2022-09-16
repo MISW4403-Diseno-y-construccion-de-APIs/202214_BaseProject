881f89b3f2338de03c900c47d596b28bc7bf219d
German Herrera <germaneherrera@gmail.com>
2022-09-16 00:17:41 -0500

    feat: agrega ciudad controller

--- src/ciudad/ciudad.module.ts
+++ src/ciudad/ciudad.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { CiudadEntity } from './entities/ciudad.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CiudadService } from './providers/ciudad.service';
+import { CiudadController } from './controllers/ciudad/ciudad.controller';
 
 @Module({
     imports: [
@@ -10,5 +11,6 @@ import { CiudadService } from './providers/ciudad.service';
         ])
     ],
     providers: [CiudadService],
+    controllers: [CiudadController],
 })
 export class CiudadModule {}
---
+++ src/ciudad/controllers/ciudad/ciudad.controller.ts
@@ -0,0 +1,47 @@
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+
+import { CiudadService } from '../../providers/ciudad.service';
+import { plainToInstance } from 'class-transformer';
+import { CiudadEntity } from '../../entities/ciudad.entity';
+import { BusinessErrorsInterceptor } from '../../../shared/interceptors/business-errors.interceptor';
+import { CiudadDto } from 'src/ciudad/dtos/ciudad.dto';
+// import { CiudadDto } from 'src/ciudad/dtos/ciudad.dto';
+
+
+@Controller('ciudades')
+@UseInterceptors(BusinessErrorsInterceptor)
+export class CiudadController {
+    constructor(
+        private readonly ciudadService: CiudadService
+    ) { }
+
+    @Get()
+    async findAll() {
+        return await this.ciudadService.findAll();
+    }
+
+    @Get(':ciudadId')
+    async findOne(@Param('ciudadId') ciudadId: string) {
+        return await this.ciudadService.findOne(ciudadId);
+    }
+
+    @Post()
+    async create(@Body() ciudadDto: CiudadDto) {
+        const ciudad: CiudadEntity = plainToInstance(CiudadEntity, ciudadDto);
+        return await this.ciudadService.create(ciudad);
+    }
+
+    @Put(':ciudadId')
+    async update(@Param('ciudadId') ciudadId: string, @Body() ciudadDto: CiudadDto) {
+        const ciudad: CiudadEntity = plainToInstance(CiudadEntity, ciudadDto);
+        return await this.ciudadService.update(ciudadId, ciudad);
+    }
+
+    @Delete(':ciudadId')
+    @HttpCode(204)
+    async delete(@Param('ciudadId') ciudadId: string) {
+        return await this.ciudadService.delete(ciudadId);
+    }
+}
+
+
