b90243442db408e95a7fd41d7d5d5260c88e66fa
German Herrera <germaneherrera@gmail.com>
2022-09-15 23:13:54 -0500

    feat: agrega configuración db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CiudadModule } from './ciudad/ciudad.module';
+import { CiudadEntity } from './ciudad/entities/ciudad.entity';
+import { SupermercadoEntity } from './supermercado/entities/supermercado.entity';
 import { SupermercadoModule } from './supermercado/supermercado.module';
 
 @Module({
-  imports: [CiudadModule, SupermercadoModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Assemblix86',
+      database: 'supermercado',
+      entities: [
+       CiudadEntity,
+       SupermercadoEntity
+      ],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true
+    }),
+    CiudadModule, SupermercadoModule],
   controllers: [AppController],
   providers: [AppService],
 })
