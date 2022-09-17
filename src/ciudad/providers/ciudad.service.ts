73b0e10d0185d792b4b103789fdde7668004f0bc
German Herrera <germaneherrera@gmail.com>
2022-09-17 14:20:51 -0500

    feat: Implementa regla de negocio país

--- src/ciudad/providers/ciudad.service.ts
+++ src/ciudad/providers/ciudad.service.ts
@@ -6,18 +6,32 @@ import { CiudadEntity } from '../entities/ciudad.entity';
 
 @Injectable()
 export class CiudadService {
+    paises = ['Argentina', 'Ecuador', 'Paraguay']; 
+
     constructor(
         @InjectRepository(CiudadEntity)
         private readonly ciudadRepository: Repository<CiudadEntity>,
     ){}
 
     public mensajeError = "No se encontró la ciudad con la identificación proporcionada.";
+    public mensajePais = "El país no se encuentra en la lista habilitada (Argentina, Ecuador, Paraguay).";
+    public mensajeId = "El id no puede ser null.";
 
     async create(ciudad: CiudadEntity): Promise<CiudadEntity> {
+        const pais = this.paises.find( element => element === ciudad.pais);
+        if(!pais){
+            throw new BusinessLogicException(this.mensajePais, BusinessError.NOT_FOUND);  
+        }
         return await this.ciudadRepository.save(ciudad);
     }
 
     async update(id: string, ciudad: CiudadEntity): Promise<CiudadEntity> {
+        this.validarPais(ciudad);
+
+        if(id === "null"){
+            throw new BusinessLogicException(this.mensajeId, BusinessError.NOT_FOUND);
+        }
+
         const persiteCiudad: CiudadEntity = await this.ciudadRepository.findOne({where:{id}});
         if (!persiteCiudad)
           throw new BusinessLogicException(this.mensajeError, BusinessError.NOT_FOUND);
@@ -27,9 +41,8 @@ export class CiudadService {
 
     async delete(id: string) {
         const ciudad: CiudadEntity = await this.ciudadRepository.findOne({where:{id}});
-        if (!ciudad)
-          throw new BusinessLogicException(this.mensajeError, BusinessError.NOT_FOUND);
-     
+        this.validarPais(ciudad);
+
         await this.ciudadRepository.remove(ciudad);
     }
 
@@ -45,5 +58,11 @@ export class CiudadService {
         return await this.ciudadRepository.find();
     }
 
+    private validarPais(ciudad: CiudadEntity) {
+        const pais = this.paises.find(element => element === ciudad.pais);
+        if (!pais) {
+            throw new BusinessLogicException(this.mensajePais, BusinessError.NOT_FOUND);
+        }
+    }
 
 }
