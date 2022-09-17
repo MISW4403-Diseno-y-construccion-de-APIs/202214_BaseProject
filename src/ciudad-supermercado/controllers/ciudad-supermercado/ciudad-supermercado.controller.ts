00f53e1f29ddf37c96c7a77dae56421f8d82e18e
German Herrera <germaneherrera@gmail.com>
2022-09-17 16:02:03 -0500

    feat: implementa addSupermarketToCity, findSupermarketsFromCity, findSupermarketFromCity

--- src/ciudad-supermercado/controllers/ciudad-supermercado/ciudad-supermercado.controller.ts
+++ src/ciudad-supermercado/controllers/ciudad-supermercado/ciudad-supermercado.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, UseInterceptors } from '@nestjs/common';
+import { CiudadSupermercadoService } from 'src/ciudad-supermercado/providers/ciudad-supermercado/ciudad-supermercado.service';
+import { BusinessErrorsInterceptor } from '../../../shared/interceptors/business-errors.interceptor';
+// @Controller('ciudad-supermercado')
+@Controller('ciudades')
+@UseInterceptors(BusinessErrorsInterceptor)
+export class CiudadSupermercadoController {
+    constructor(
+        private readonly ciudadSupermercadoService: CiudadSupermercadoService
+    ) { }
+    @Post(':ciudadId/supermercados/:supermercadoId')
+    async addSupermarketToCity(@Param('ciudadId') ciudadId: string, @Param('supermercadoId') supermercadoId: string){
+        return this.ciudadSupermercadoService.addSupermarketToCity(supermercadoId, ciudadId);
+    }
+    
+    @Get(':ciudadId/supermercados')
+    findSupermarketsFromCity(@Param('ciudadId') ciudadId: string){
+        return this.ciudadSupermercadoService.findSupermarketsFromCity(ciudadId);
+    }
 
-@Controller('ciudad-supermercado')
-export class CiudadSupermercadoController {}
+    @Get(':ciudadId/supermercados/:supermercadoId')
+    findSupermarketFromCity(@Param('ciudadId') ciudadId: string, @Param('supermercadoId') supermercadoId: string){
+        return this.ciudadSupermercadoService.findSupermarketFromCity(supermercadoId, ciudadId);
+    }
+
+    updateSupermarketsFromCity(){}
+    deleteSupermarketFromCity(){}
+}
--- src/ciudad-supermercado/providers/ciudad-supermercado/ciudad-supermercado.service.ts
+++ src/ciudad-supermercado/providers/ciudad-supermercado/ciudad-supermercado.service.ts
@@ -1,4 +1,75 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CiudadEntity } from '../../../ciudad/entities/ciudad.entity';
+import { SupermercadoEntity } from '../../../supermercado/entities/supermercado.entity';
+import { Repository } from 'typeorm';
+import { BusinessError, BusinessLogicException } from '../../../shared/errors/business-errors';
+
 
 @Injectable()
-export class CiudadSupermercadoService {}
+export class CiudadSupermercadoService {
+    public mensajeErrorCiudad = "No se encontró la ciudad con la identificación proporcionada.";
+    public mensajeErrorSupermercado = "No se encontró el supermercado con la identificación proporcionada.";
+    public mensajeId = "El id no puede ser null.";
+
+    constructor(
+        @InjectRepository(SupermercadoEntity)
+        private readonly supermercadoRepository: Repository<SupermercadoEntity>,
+     
+        @InjectRepository(CiudadEntity)
+        private readonly ciudadRepository: Repository<CiudadEntity>
+    ) {}
+
+
+    //Asociar un supermercado a una ciudad.
+    async addSupermarketToCity(supermercadoId:string, ciudadId:string){
+        const ciudad: CiudadEntity =  await this.ciudadRepository.findOne({where:{id:ciudadId},  relations:["supermercados"] });
+        if(!ciudad)
+            throw new BusinessLogicException(this.mensajeErrorCiudad, BusinessError.NOT_FOUND);
+
+        const supermercado: SupermercadoEntity =  await this.supermercadoRepository.findOne({where:{id: supermercadoId}, });
+        if(!supermercado)
+            throw new BusinessLogicException(this.mensajeErrorSupermercado, BusinessError.NOT_FOUND);
+
+        const searchSupermercado = ciudad.supermercados.find( (item) => item.id === supermercado.id);
+        if(searchSupermercado){
+            throw new BusinessLogicException("El supermercado ya se encuentra vinculado.", BusinessError.NOT_FOUND);
+        }
+
+        ciudad.supermercados = [...ciudad.supermercados, supermercado];
+        return await this.ciudadRepository.save(ciudad);        
+    }
+
+    //Obtener los supermercados que tiene una ciudad.
+    async findSupermarketsFromCity(ciudadId:string){
+        const ciudad: CiudadEntity =  await this.ciudadRepository.findOne({where:{id:ciudadId},  relations:["supermercados"] });
+        if(!ciudad)
+            throw new BusinessLogicException(this.mensajeErrorCiudad, BusinessError.NOT_FOUND);
+
+        return ciudad.supermercados;
+
+    }
+    //Obtener un supermercado de una ciudad.
+    async findSupermarketFromCity(supermercadoId:string, ciudadId:string){
+        const ciudad: CiudadEntity =  await this.ciudadRepository.findOne({where:{id:ciudadId},  relations:["supermercados"] });
+        if(!ciudad)
+            throw new BusinessLogicException(this.mensajeErrorCiudad, BusinessError.NOT_FOUND);
+
+        const searchSupermercado = ciudad.supermercados.find( (item) => item.id === supermercadoId);
+        if(!searchSupermercado){
+            throw new BusinessLogicException(this.mensajeErrorSupermercado, BusinessError.NOT_FOUND);
+        }
+
+        return searchSupermercado;
+  
+    }
+    //Actualizar los supermercados que tiene una ciudad.
+    updateSupermarketsFromCity(){
+        
+    }
+    //Eliminar el supermercado que tiene una ciudad.
+    deleteSupermarketFromCity(){
+        
+    }
+
+}
