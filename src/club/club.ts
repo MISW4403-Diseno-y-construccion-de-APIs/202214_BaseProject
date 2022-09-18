fb37e458878ed5bc3bcaf37bc3c5874b939014b5
ChristianLoza <Christianlzap@gmail.com>
2022-09-18 10:36:08 -0500

    Added DTO

---
+++ src/club/club.ts
@@ -0,0 +1,19 @@
+import {IsDateString, IsNotEmpty, IsString, IsUrl} from 'class-validator';
+
+export class Club {
+    @IsString()
+    @IsNotEmpty()
+    nombre: string;
+
+    @IsUrl()
+    @IsNotEmpty()
+    imagen: string;
+
+    @IsString()
+    @IsNotEmpty()
+    descripcion: string;
+
+    @IsDateString()
+    @IsNotEmpty()
+    fecha_fundacion: string;
+}
