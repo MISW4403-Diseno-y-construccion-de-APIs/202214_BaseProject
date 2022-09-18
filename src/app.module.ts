e4080c0d318906cae138f21083f7b9973bb6c8f1
ChristianLoza <Christianlzap@gmail.com>
2022-09-18 10:48:36 -0500

    Added db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SocioModule } from './socio/socio.module';
+import { ClubModule } from './club/club.module';
+import { ClubEntity } from './club/club.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    SocioModule,
+    ClubModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'clubdb',
+      entities: [ClubEntity],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
